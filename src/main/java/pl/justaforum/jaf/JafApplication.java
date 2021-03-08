6ff33952587bc05d3ca115135aabab4e48e999a1
Remigiusz Ratajski <remik.ratajski@gmail.com>
2021-03-08 18:39:03 +0100

    Pierwszy commit

---
+++ src/main/java/pl/justaforum/jaf/JafApplication.java
@@ -0,0 +1,13 @@
+package pl.justaforum.jaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JafApplication.class, args);
+    }
+
+}
