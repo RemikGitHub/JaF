86bc536955e0e95ad0de01c6e90d3b4954e99112
Remigiusz Ratajski <remik.ratajski@gmail.com>
2021-03-15 22:49:45 +0100

    Usuniecie wrazliwych danych

--- src/main/java/pl/justaforum/jaf/config/SecurityConfig.java
+++ src/main/java/pl/justaforum/jaf/config/SecurityConfig.java
@@ -1,12 +1,10 @@
 package pl.justaforum.jaf.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.justaforum.jaf.users.UserService;
 
